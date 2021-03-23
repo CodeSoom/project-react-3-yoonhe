d16881b157d30ee09784b728cfcc259bfc26e8ef
yoonhe <yhehe228@gmail.com>
2021-03-23 20:53:52 +0900

    개발 환경을 구축합니다

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>Hello, Room View</p>
+    </div>
+  );
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app'),
+);
