61639ce70f319ed7dec3596b73188ff72c968009
yoonhe <yhehe228@gmail.com>
2021-03-24 01:36:12 +0900

    초기 셋팅을 변경합니다

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  mode: 'development',
   module: {
     rules: [
       {
@@ -11,9 +15,28 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
   devServer: {
     historyApiFallback: {
       index: 'index.html',
     },
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
