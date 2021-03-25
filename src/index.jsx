bb53182c7d2a39fa302d3c4d42bf919b7a6d31e0
yoonhe <yhehe228@gmail.com>
2021-03-25 15:44:03 +0900

    충돌난 파일들을 머지합니다

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,13 @@ import ReactDom from 'react-dom';
 function App() {
   return (
     <div>
-      <p>Hello, Room View</p>
+      <p>룸프리뷰 !</p>
+      <p>더이상 속지마세요! 직접 보고 결정하세요</p>
     </div>
   );
 }
 
 ReactDom.render(
   <App />,
-  document.getElementById('app'),
+  document.getElementById('root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
@@ -11,4 +21,16 @@ module.exports = {
       },
     ],
   },
+  mode: 'development',
+  devServer: {
+    port: 7777,
+    contentBase: path.join(__dirname, 'public/'),
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
