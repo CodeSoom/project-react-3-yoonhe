a7c58f472a3cfd1cec69f496365b630a754f1867
yoonhe <yhehe228@gmail.com>
2021-03-28 09:27:16 +0900

    BrowserRouter basename test

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 ReactDom.render(
   <Provider store={store}>
-    <BrowserRouter basename={isProduction ? 'https://yoonhe.github.io/project-react-3-yoonhe/' : '/'}>
+    <BrowserRouter basename={isProduction ? '/project-react-3-yoonhe/' : '/'}>
       <App />
     </BrowserRouter>
   </Provider>,
