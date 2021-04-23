dd8a506b5294349c5088754058522b361b8ac476
yoonhe <yhehe228@gmail.com>
2021-04-23 09:36:51 +0900

    로그아웃 상태에서 방 등록 페이지 접근시 "로그인이 필요한 페이지 입니다" 메시지를 노출시킵니다
    로그아웃 상태일때는 유저에게 방 등록 권한이 없으므로
    방 등록 폼을 노출시키지않고 로그인이 필요한 페이지라는 것을 알립니다

--- src/container/AddRoomContainer.test.jsx
+++ src/container/AddRoomContainer.test.jsx
@@ -16,6 +16,7 @@ describe('AddRoomContainer', () => {
     jest.resetAllMocks();
 
     useSelector.mockImplementation((selector) => selector({
+      isLoggedIn: given.isLoggedIn || false,
       addRoomFields: {
         address: '',
         moveInType: '',
@@ -33,139 +34,153 @@ describe('AddRoomContainer', () => {
     return render(<AddRoomContainer onGoToMain={handleGoToMain} />);
   }
 
-  it('renders addRoom title', () => {
-    const { queryByText } = renderAddRoomContainer();
+  context('when logged in', () => {
+    given('isLoggedIn', () => true);
 
-    expect(queryByText('살았던 혹은 살고계신 방을 알려주세요 😊')).not.toBeNull();
-  });
+    it('renders addRoom title', () => {
+      const { queryByText } = renderAddRoomContainer();
 
-  it('renders input text controls', () => {
-    const { queryByLabelText } = renderAddRoomContainer();
-
-    const labels = ['주소', '입주 유형', '보증금', '월세', '관리비'];
-
-    labels.forEach((label) => {
-      expect(queryByLabelText(label)).not.toBeNull();
-    });
-  });
-
-  it('renders input radio controls', () => {
-    const { queryByText, queryAllByLabelText } = renderAddRoomContainer();
-
-    const categories = ['채광', '통풍', '습기', '해충', '소음'];
-    const scores = [1, 2, 3, 4, 5];
-
-    categories.forEach((category) => {
-      expect(queryByText(category)).not.toBeNull();
+      expect(queryByText('살았던 혹은 살고계신 방을 알려주세요 😊')).not.toBeNull();
     });
 
-    scores.forEach((score) => {
-      expect(queryAllByLabelText(score)).not.toBeNull();
-    });
-  });
+    it('renders input text controls', () => {
+      const { queryByLabelText } = renderAddRoomContainer();
 
-  it('calls onChange handler when input change', () => {
-    const { queryByLabelText } = renderAddRoomContainer();
+      const labels = ['주소', '입주 유형', '보증금', '월세', '관리비'];
 
-    const controls = [
-      {
-        label: '주소',
-        name: 'address',
-        value: '서울시 강남구 역삼동',
-      },
-      {
-        label: '입주 유형',
-        name: 'moveInType',
-        value: '전세',
-      },
-      {
-        label: '보증금',
-        name: 'deposit',
-        value: '10000',
-      },
-      {
-        label: '월세',
-        name: 'monthlyRent',
-        value: '0',
-      },
-      {
-        label: '관리비',
-        name: 'adminCost',
-        value: '10',
-      },
-    ];
-
-    controls.forEach(({ label, name, value }) => {
-      fireEvent.change(queryByLabelText(label), {
-        target: {
-          value,
-        },
-      });
-
-      expect(dispatch).toBeCalledWith({
-        type: 'roomPreviews/changeAddRoomFields',
-        payload: {
-          name,
-          value,
-        },
+      labels.forEach((label) => {
+        expect(queryByLabelText(label)).not.toBeNull();
       });
     });
-  });
 
-  it('renders image upload control', () => {
-    const { queryByText } = renderAddRoomContainer();
+    it('renders input radio controls', () => {
+      const { queryByText, queryAllByLabelText } = renderAddRoomContainer();
 
-    expect(queryByText('방 사진 등록하기')).not.toBeNull();
-  });
+      const categories = ['채광', '통풍', '습기', '해충', '소음'];
+      const scores = [1, 2, 3, 4, 5];
 
-  it('calls onChange handler when input file change', async () => {
-    const { getByLabelText } = renderAddRoomContainer();
+      categories.forEach((category) => {
+        expect(queryByText(category)).not.toBeNull();
+      });
 
-    await waitFor(() => {
-      fireEvent.change(getByLabelText('방 사진 등록하기'), {
-        target: {
-          files: {
-            0: {
-              name: 'test.png',
+      scores.forEach((score) => {
+        expect(queryAllByLabelText(score)).not.toBeNull();
+      });
+    });
+
+    it('calls onChange handler when input change', () => {
+      const { queryByLabelText } = renderAddRoomContainer();
+
+      const controls = [
+        {
+          label: '주소',
+          name: 'address',
+          value: '서울시 강남구 역삼동',
+        },
+        {
+          label: '입주 유형',
+          name: 'moveInType',
+          value: '전세',
+        },
+        {
+          label: '보증금',
+          name: 'deposit',
+          value: '10000',
+        },
+        {
+          label: '월세',
+          name: 'monthlyRent',
+          value: '0',
+        },
+        {
+          label: '관리비',
+          name: 'adminCost',
+          value: '10',
+        },
+      ];
+
+      controls.forEach(({ label, name, value }) => {
+        fireEvent.change(queryByLabelText(label), {
+          target: {
+            value,
+          },
+        });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'roomPreviews/changeAddRoomFields',
+          payload: {
+            name,
+            value,
+          },
+        });
+      });
+    });
+
+    it('renders image upload control', () => {
+      const { queryByText } = renderAddRoomContainer();
+
+      expect(queryByText('방 사진 등록하기')).not.toBeNull();
+    });
+
+    it('calls onChange handler when input file change', async () => {
+      const { getByLabelText } = renderAddRoomContainer();
+
+      await waitFor(() => {
+        fireEvent.change(getByLabelText('방 사진 등록하기'), {
+          target: {
+            files: {
+              0: {
+                name: 'test.png',
+              },
             },
           },
-        },
+        });
+      });
+
+      expect(dispatch).toBeCalled();
+    });
+
+    it('renders "등록하기" button', () => {
+      const { queryByText } = renderAddRoomContainer();
+
+      expect(queryByText('등록하기')).not.toBeNull();
+    });
+
+    it('calls onSubmit handler when "등록하기" button click', () => {
+      const { getByText } = renderAddRoomContainer();
+
+      fireEvent.click(getByText('등록하기'));
+
+      expect(dispatch).toBeCalled();
+      expect(handleGoToMain).toBeCalled();
+    });
+
+    context('with upload images', () => {
+      given('images', () => ['IMAGE_URL_1', 'IMAGE_URL_2']);
+      it('renders room images', () => {
+        const { queryAllByTitle } = renderAddRoomContainer();
+
+        expect(queryAllByTitle('방 사진')).toHaveLength(2);
       });
     });
 
-    expect(dispatch).toBeCalled();
-  });
+    context('without upload images', () => {
+      given('images', () => []);
+      it('renders room images', () => {
+        const { queryByTitle } = renderAddRoomContainer();
 
-  it('renders "등록하기" button', () => {
-    const { queryByText } = renderAddRoomContainer();
-
-    expect(queryByText('등록하기')).not.toBeNull();
-  });
-
-  it('calls onSubmit handler when "등록하기" button click', () => {
-    const { getByText } = renderAddRoomContainer();
-
-    fireEvent.click(getByText('등록하기'));
-
-    expect(dispatch).toBeCalled();
-    expect(handleGoToMain).toBeCalled();
-  });
-
-  context('with upload images', () => {
-    given('images', () => ['IMAGE_URL_1', 'IMAGE_URL_2']);
-    it('renders room images', () => {
-      const { queryAllByTitle } = renderAddRoomContainer();
-
-      expect(queryAllByTitle('방 사진')).toHaveLength(2);
+        expect(queryByTitle('방 사진')).toBeNull();
+      });
     });
   });
 
-  context('without upload images', () => {
-    given('images', () => []);
-    it('renders room images', () => {
-      const { queryByTitle } = renderAddRoomContainer();
+  context('when logged out', () => {
+    given('isLoggedIn', () => false);
 
-      expect(queryByTitle('방 사진')).toBeNull();
+    it('renders login request message', () => {
+      const { getByText } = renderAddRoomContainer();
+
+      expect(getByText('로그인이 필요한 페이지 입니다')).not.toBeNull();
     });
   });
 });
