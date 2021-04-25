e6bc0312a94289ced3e619b997a1da65d853c99a
yoonhe <yhehe228@gmail.com>
2021-04-25 09:57:03 +0900

    방등록 페이지에서 보증금, 월세, 관리 인풋에 단위를 표시하고 입주 유형을 셀렉트 박스로 선택이 가능하게합니다
    보증금, 월세, 관리비를 만원 단위로 입력받도록 합니다
    입주 유형을 유저마다 다르게 입력할 수 있으므로 셀렉트 박스를 제공하여 선택할 수 있도록 합니다

--- src/components/TextField.test.jsx
+++ src/components/TextField.test.jsx
@@ -13,28 +13,92 @@ describe('TextField', () => {
 
   function renderTextField() {
     return render(<TextField
-      label="주소"
-      type="text"
-      name="address"
+      label={given.inputLabel}
+      type={given.inputType}
+      name={given.inputName}
+      selectOptions={given.selectOptions}
+      unit={given.inputUnit}
       onChange={handleChange}
     />);
   }
 
-  it('renders input text control', () => {
-    const { queryByLabelText } = renderTextField();
-
-    expect(queryByLabelText('주소')).not.toBeNull();
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
-  it('calls onChange handler when input change', () => {
-    const { queryByLabelText } = renderTextField();
+  context('when formType is "input"', () => {
+    given('inputLabel', () => '월세');
+    given('inputType', () => 'text');
+    given('inputName', () => 'monthlyRent');
+    given('inputUnit', () => '만원');
 
-    fireEvent.change(queryByLabelText('주소'), {
-      target: {
-        value: '서울시 강남구',
-      },
+    it('renders input text control', () => {
+      const { getByLabelText, queryByText } = renderTextField();
+
+      expect(getByLabelText('월세')).not.toBeNull();
+      expect(queryByText('만원')).not.toBeNull();
     });
 
-    expect(handleChange).toBeCalled();
+    it('calls onChange handler when input change', () => {
+      const { getByLabelText } = renderTextField();
+
+      fireEvent.change(getByLabelText('월세'), {
+        target: {
+          value: 1000,
+        },
+      });
+
+      expect(handleChange).toBeCalled();
+    });
+  });
+
+  context('when formType is "textarea"', () => {
+    given('inputLabel', () => '주소');
+    given('inputType', () => 'textarea');
+    given('inputName', () => 'address');
+
+    it('renders textarea control', () => {
+      const { queryByLabelText } = renderTextField();
+
+      expect(queryByLabelText('주소')).not.toBeNull();
+    });
+
+    it('calls onChange handler when textarea change', () => {
+      const { getByLabelText } = renderTextField();
+
+      fireEvent.change(getByLabelText('주소'), {
+        target: {
+          value: '서울시 강남구',
+        },
+      });
+
+      expect(handleChange).toBeCalled();
+    });
+  });
+
+  context('when formType is "select"', () => {
+    given('inputLabel', () => '입주유형');
+    given('inputType', () => 'select');
+    given('inputName', () => 'moveInType');
+    given('selectOptions', () => ['전세', '월세']);
+
+    it('renders select control', () => {
+      const { queryByText } = renderTextField();
+
+      expect(queryByText('전세')).not.toBeNull();
+      expect(queryByText('월세')).not.toBeNull();
+    });
+
+    it('calls onChange handler when select change', () => {
+      const { getByLabelText } = renderTextField();
+
+      fireEvent.change(getByLabelText('입주유형'), {
+        target: {
+          value: '월세',
+        },
+      });
+
+      expect(handleChange).toBeCalled();
+    });
   });
 });
